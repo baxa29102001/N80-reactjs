ea1b9174bb6d3b22b380fbfe014b4c38f3d30a36
unknown <mr_baxa>
2023-02-04 18:04:21 +0500

    Fix

--- src/App.js
+++ src/App.js
@@ -1,56 +1,7 @@
-import { useState } from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Link,
-  Routes,
-  Route,
-} from "react-router-dom";
-import "./App.css";
-import Example from "./components/Example";
-import SingleProduct from "./components/SingleProduct";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-
-//     element: (
-//       <>
-//         <h1>Main page</h1>
-//         <Link to={"/helloWorld"}>Hello Page</Link>
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/helloWorld",
-
-//     element: <h2>Hello page</h2>,
-//   },
-// ]);
+import React from "react";
 
 function App() {
-  return (
-    <>
-      <div className="">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <h1>Main page</h1>
-                <Link to={"/products"}>Products Page</Link>
-              </>
-            }
-          />
-
-          <Route path="/products" element={<Example />} />
-          <Route path="/products/:id" element={<SingleProduct />} />
-        </Routes>
-
-        {/* <RouterProvider router={router} /> */}
-      </div>
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
